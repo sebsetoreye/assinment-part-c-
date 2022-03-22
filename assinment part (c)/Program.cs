13e70958729aa85c3a8c36f7efc94b23b5bccc08
sebas <sebas@DESKTOP-SEBASTI>
2022-03-23 10:23:09 +1100

    Add project files.

---
+++ assinment part (c)/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int WL;
+
+WL = 0;
+
+string WC, aw;
+
+Console.Write("Please Input You're String : ");
+WC = Console.ReadLine();
+
+
+string[] words = WC.Split(' ');
+
+foreach (var word in words)
+{
+    WL += 1;
+    Console.WriteLine(WL);
+    if (WL == 8)
+    {
+        goto ASCII_code;
+    }
+    else if (WL == 9)
+    {
+        Console.WriteLine("This String is to big");
+        System.Environment.Exit(0);
+    }
+}
+ASCII_code:
+Console.WriteLine("hello");
